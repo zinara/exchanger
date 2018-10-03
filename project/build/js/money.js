7f3912d2aff3a270ba8c6dc56389969a646652a1
sochima <ozojiez@gmail.com>
2018-10-03 16:38:16 +0100

    trying yet another github link

--- project/build/js/money.js
+++ project/build/js/money.js
@@ -43,7 +43,7 @@
         app.networkState();
       })
 
-  const newcountriesUrl = 'build/js/newcountries.json';
+  const newcountriesUrl = 'exchanger/project/build/js/newcountries.json';
 
   // add object files to the currency-from objectstore
   
